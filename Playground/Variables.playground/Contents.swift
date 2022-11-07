c99628fcf42159638faac44e99639d463a538f53
Phillip Reynolds <preynolds@atp.com>
2022-11-07 13:52:40 -0600

    Completed Module 3 of course.

---
+++ Playground/Variables.playground/Contents.swift
@@ -0,0 +1,11 @@
+var user: String = "Michael Scott"
+var calories: Int = 2400
+
+print(user)
+print(calories)
+
+user = "Dwight Schrute"
+print(user)
+
+let dailyCaloriesWomen: Int = 2000
+let dailyCaloriesMen: Int = 2500
